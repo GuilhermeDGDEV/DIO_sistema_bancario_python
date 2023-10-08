990758e3207a017929dea6d09a6a15f05c9860ff
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2023-10-08 18:52:00 -0300

    Implementando a logica de saque, deposito e extrato

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from utils import isfloat
+
 menu = '''
 
 [d] Depositar
@@ -18,16 +20,48 @@ while True:
     opcao = input(menu).lower()
 
     if opcao == 'd':
-        print('Depósito')
+        valor = input('Informe o valor do depósito: ')
+
+        if isfloat(valor) and float(valor) > 0:
+            valor = float(valor)
+            saldo += valor
+            extrato += f'Depósito: R$ {valor:.2f}\n'
+        else:
+            print('Operação falhou! O valor informado é inválido.')
 
     elif opcao == 's':
-        print('Saque')
+        valor = input('Informe o valor do saque: ')
+
+        if isfloat(valor) and float(valor) > 0:
+            
+            valor = float(valor)
+
+            excedeu_saldo = valor > saldo
+            excedeu_limite = valor > limite
+            excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+            if excedeu_saldo:
+                print('Operação falhou! Você não tem saldo suficiente.')
+            elif excedeu_limite:
+                print('Operação falhou! O valor do saque excede o limite.')
+            elif excedeu_saques:
+                print('Operação falhou! Número de saques excedido.')
+            else:
+                saldo -= valor
+                extrato += f'Saque: R$ {valor:.2f}\n'
+                numero_saques += 1
+
+        else:
+            print('Operação falhou! O valor informado é inválido.')
 
     elif opcao == 'e':
-        print('Extrato')
+        print(' EXTRATO '.center(50, '='))
+        print('Não foram realizadas movimentações.' if not extrato else extrato)
+        print(f'\nSaldo: R$ {saldo:.2f}')
+        print('=' * 50)
 
     elif opcao == 'q':
         break
     
     else:
-        print('Operação inválida, por favor selecione novamente a operação desejada')
+        print('Operação inválida, por favor selecione novamente a operação desejada.')
